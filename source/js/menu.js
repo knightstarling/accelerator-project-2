e619799ddfe9402520ddb0176a2170b2b3982d52
Kristina Lingthep <kris.lingthep@gmail.com>
2025-01-19 17:44:43 +0300

    добавляет фавиконки, фоновые изображения и состояния элементов

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,6 +1,15 @@
+const body = document.querySelector('.page__body');
 const nav = document.querySelector('.header__navigation');
 const navToggle = document.querySelector('.header__button');
+const overlay = document.querySelector('.page__overlay');
 
 navToggle.addEventListener('click', () => {
   nav.classList.toggle('header__navigation--open');
+  body.classList.toggle('page__body--lock-scroll');
+
+  if (nav.classList.contains('header__navigation--open')) {
+    overlay.style.display = 'block';
+  } else {
+    overlay.style.display = 'none';
+  }
 });
